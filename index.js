96d3ba89deb3d87b93a0682ba03bc9fdfc330edb
Jason Wong <wongkj.11@gmail.com>
2025-08-22 06:08:51 +1000

    feat: Modifications BREAKING CHANGE: Added a comment

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-const app = require('./app')
+const app = require("./app");
 
 const port = process.env.PORT || 3000;
 
+// comment
+
 app.listen(port, () => {
-    console.log(`listening on port ${port}`);
-})
+  console.log(`listening on port ${port}`);
+});
